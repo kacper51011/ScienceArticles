19c97dcb3037daf452ffcce7fd9da881b570ae06
Kacper <kacper.tylec1999@gmail.com>
2024-02-01 11:24:53 +0100

    Add project files.

---
+++ ScienceArticles.API/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ScienceArticles.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+    }
+}
