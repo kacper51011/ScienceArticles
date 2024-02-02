27c3d67b02254e3b968e50e0df5b7d75aabb0280
Kacper <kacper.tylec1999@gmail.com>
2024-02-02 13:13:35 +0100

    Connected to europePMC soap web service

--- ScienceArticles.Infrastructure/Db/ApplicationDbContext.cs
+++ ScienceArticles.Infrastructure/Db/ApplicationDbContext.cs
@@ -9,6 +9,9 @@ namespace ScienceArticles.Infrastructure.Db
 {
     public class ApplicationDbContext: DbContext
     {
-
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
     }
 }
