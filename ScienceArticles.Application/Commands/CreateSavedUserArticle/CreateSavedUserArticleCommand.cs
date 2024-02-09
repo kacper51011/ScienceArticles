18b02f800e3fd89af23e23e7cadd4e2865e91ea2
Kacper <kacper.tylec1999@gmail.com>
2024-02-09 13:01:45 +0100

    CreateSavedUserArticleCommand

---
+++ ScienceArticles.Application/Commands/CreateSavedUserArticle/CreateSavedUserArticleCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using ScienceArticles.Application.Dtos.CreateSavedUserArticle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScienceArticles.Application.Commands.CreateSavedUserArticle
+{
+    public record CreateSavedUserArticleCommand(CreateSavedUserRequestDto dto): IRequest
+    {
+    }
+}
---
+++ ScienceArticles.Application/Commands/CreateSavedUserArticle/CreateSavedUserArticleCommandHandler.cs
@@ -0,0 +1,57 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using ScienceArticles.Application.Services;
+using ScienceArticles.Domain.Entities;
+using ScienceArticles.Domain.Interfaces;
+using ScienceArticles.Domain.ValueObjects;
+
+namespace ScienceArticles.Application.Commands.CreateSavedUserArticle
+{
+    public class CreateSavedUserArticleCommandHandler : IRequestHandler<CreateSavedUserArticleCommand>
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+        private readonly IArticleRepository _articleRepository;
+        private readonly IEuropePMCService _europePMCService;
+
+        public CreateSavedUserArticleCommandHandler(IHttpContextAccessor contextAccessor, IArticleRepository articleRepository, IEuropePMCService europePMCService)
+        {
+            _contextAccessor = contextAccessor;
+            _articleRepository = articleRepository;
+            _europePMCService = europePMCService;
+
+
+
+        }
+        public async Task Handle(CreateSavedUserArticleCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+
+                var userId = _contextAccessor.HttpContext.User.Claims.FirstOrDefault(x => x.Type == "sub")?.Value;
+                if (userId == null)
+                {
+                    throw new ArgumentNullException("UserId not specified");
+                }
+
+                var publication = await _europePMCService.FindPublicationById(request.dto.PublicationId);
+                if (publication == null)
+                {
+                    throw new ArgumentNullException("couldn`t find specified publication");
+                }
+
+                var ArticleToSave = Article.Create(publication.PublicationId, publication.Title, publication.PublicationYear, publication.Abstract, publication.TextLink, new UserId(Guid.Parse(userId)) , new CategoryId(request.dto.CategoryId));
+
+                await _articleRepository.SaveArticle(ArticleToSave);
+
+                return;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+    }
+}
